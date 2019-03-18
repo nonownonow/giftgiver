c014e274f308eab8d4f30b0f71bd21030bd6b16b
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-03-18 18:47:46 +0900

    2-11

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
 class App extends Component {
   constructor () {
@@ -6,10 +7,18 @@ class App extends Component {
     this.state = { gifts: [] }
   }
 
+  addGift = () => {
+    const { gifts } = this.state
+    const maxId = Math.max(...gifts.map(v=>v.id))
+    const id = gifts.length!==0? maxId:0
+    gifts.push({ id : id + 1})
+  }
+
   render () {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <Button className='btn-add' onClick={this.addGift}></Button>
       </div>
     )
   }
