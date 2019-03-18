d3e888454a6856365753a1b9a997da707ece69d0
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-03-18 18:59:51 +0900

    -

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -16,3 +16,8 @@ it('adds a new gift to `state` when clicking  `add gift` button', ()=>{
   app.find('.btn-add').simulate('click')
   expect(app.state().gifts).toEqual([{id:1}])
 })
+
+it('adds a new gift to the rendered list when clicking the `add gift`', ()=>{
+  app.find('.btn-add').simulate('click')
+  expect(app.find('gift-list').children().length).toEqual(2)
+})
