a66a29ecc8cf43539fe18810e30d950a82e2c90f
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-03-18 17:37:57 +0900

    tdd start

---
+++ src/components/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+
+const app = shallow(<App />)
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot()
+})
+
+it('initializes the `state` with an empty list of gifts', ()=>{
+  expect(app.state().gifts).toEqual([])
+})
